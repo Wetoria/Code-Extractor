adf121d62ab4f9905e16fac7782c60608e511cb7
Vip <vwetoria@gmail.com>
2023-08-10 11:41:36 +0800

    remove no use code

--- utils/Command.mjs
+++ utils/Command.mjs
@@ -2,10 +2,6 @@ const args = process.argv.slice(2);
 
 export const isDebug = args.includes('--debug');
 
-export const isLog = args.includes('--log');
-
-export const filterComment = args.includes('--filterComment');
-
 export const isDebugingInVSCode = process.env.TERM_PROGRAM === 'vscode';
 export const isNotDebugingInVSCode = !isDebugingInVSCode;
 
