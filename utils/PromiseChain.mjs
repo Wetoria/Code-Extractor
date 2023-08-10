0f6097ad30f41567e798964b1bb129c222f029ea
Vip <vwetoria@gmail.com>
2023-08-10 14:20:00 +0800

    Chain hack Funtion to break off or pass data

--- utils/PromiseChain.mjs
+++ utils/PromiseChain.mjs
@@ -1,8 +1,12 @@
+import {
+  colorize,
+} from './index.mjs'
+
 export async function promiseChainExcutor(funcList, initialArgs) {
   let prevResults = initialArgs
   let func = funcList.shift()
   while(func) {
-    console.log(`Excuting ${func.name || func.__getName()}`)
+    console.log(`${colorize.green('Excuting')} ${func.name} ...`)
     prevResults = await func(prevResults)
     
     // You can break off the chain by retrun false
@@ -11,4 +15,35 @@ export async function promiseChainExcutor(funcList, initialArgs) {
     func = funcList.shift()
   }
   return prevResults
+}
+
+
+function PassChainDataWrapper(fn) {
+  let key = `${fn.name} with PassChainDataWrapper`
+  const obj = {
+    [key]: (fileLineList) => {
+      fn(fileLineList)
+      return fileLineList
+    }
+  }
+  return obj[key]
+}
+
+function BreakOffChainWrapper(fn) {
+  let key = `${fn.name} with BreakOffChainWrapper`
+  const obj = {
+    [key]: (fileLineList) => {
+      fn(fileLineList)
+      return false
+    }
+  }
+  return obj[key]
+}
+
+Function.prototype.wrapperPassChainData = function() {
+  return PassChainDataWrapper(this)
+}
+
+Function.prototype.wrapperBreakOffChain = function (arr) {
+  return BreakOffChainWrapper(this)
 }
