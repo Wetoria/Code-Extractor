0d4bcf34fa8f66814a569e8a940e9c8ef60c0901
Vip <vwetoria@gmail.com>
2023-08-10 12:50:02 +0800

    log func name in process

--- utils/PromiseChain.mjs
+++ utils/PromiseChain.mjs
@@ -2,6 +2,7 @@ export async function promiseChainExcutor(funcList, initialArgs) {
   let prevResults = initialArgs
   let func = funcList.shift()
   while(func) {
+    console.log(`Excuting ${func.name || func.__getName()}`)
     prevResults = await func(prevResults)
     
     // You can break off the chain by retrun false
